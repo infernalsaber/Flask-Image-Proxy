70666c0253197fa312dfd8243fff09299885eab7
saber <72517486+infernalsaber@users.noreply.github.com>
2025-02-01 17:06:32 +0000

    Rotating user agents

--- api/index.py
+++ api/index.py
@@ -1,11 +1,19 @@
 from flask import Flask, jsonify, request, Response
-# from PIL import Image
-# import numpy as np
-# import cv2
-# import io
 import requests
+import random
 
 import base64
+
+
+user_agents = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.1 Safari/605.1.15',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0',
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1',
+    'Mozilla/5.0 (Linux; Android 10; SM-A505FN) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.120 Mobile Safari/537.36'
+]
+
+
 
 app = Flask(__name__)
 
@@ -26,12 +34,16 @@ def proxy():
         return "Image URL parameter missing", 400
     
     try:
-        response = requests.get(image_url, stream=True)
+        # Randomly select a user agent
+        headers = {
+            'User-Agent': random.choice(user_agents)
+        }
+        response = requests.get(image_url, stream=True, headers=headers)
         if response.status_code == 200:
             headers = {'Content-Type': response.headers['Content-Type']}
             return Response(response.iter_content(chunk_size=8192), headers=headers)
         else:
-            return "Image not found", 404
+            return "Can't fetch", response.status_code
     except Exception as e:
         return str(e), 500
 
